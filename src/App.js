ca98c24f1eede7a7c17e950ced932d0edaa1abc1
Богдан Исаченко <bogdan_isachenko@MacBook-Air-Bogdan.local>
2023-09-28 14:09:11 +0300

    feat: added input

--- src/App.js
+++ src/App.js
@@ -1,22 +1,38 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
 
 function App() {
+  const [text, setText] = useState('');
+
+  function handleInput(e) {
+    setText(e.target.value);
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    fetch(`http://localhost:8080/test/${text}`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+      .then((res) => res.json())
+      .then((res) => console.log(res))
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            value={text}
+            onChange={handleInput}
+          />
+        </form>
       </header>
     </div>
   );
